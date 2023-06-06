8a1f05ec587dcc10d6038e6ac1c29228f6f3fb62
solo <soloman.err@.com>
2023-06-07 01:55:59 +0600

    Fix: removed node module committing error

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 2000;
 
 app.get("/", (req, res) => {
     res.send("old-school");
-});
+})
 
 app.listen(port, ()=>{
     console.log(`Old-school listening on port: ${port}`);
-});
+})
