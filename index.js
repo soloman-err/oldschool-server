b130afb2bc5e21b8d80df99f81b90eefec0d15cb
solo <soloman.err@.com>
2023-06-06 22:30:11 +0600

    Initial: Oldschool server on-air

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 2000;
+
+app.get("/", (req, res) => {
+    res.send("old-school");
+});
+
+app.listen(port, ()=>{
+    console.log(`Old-school listening on port: ${port}`);
+});
